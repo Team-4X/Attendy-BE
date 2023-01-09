3522ef82eedf0a061043fc353819ec4390de8360
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-01-09 19:29:24 +0530

    minor edits

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,4 +1,5 @@
 exports.index = async(req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     console.log('howdy from test controller!');
+    res.send("<h1>Howdy from test Route</h1>");
 }
