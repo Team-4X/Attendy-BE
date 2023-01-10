6fcc66eae469821ee4c2b553112d63fd6c0f3213
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-01-10 19:26:27 +0530

    fixed mongoose creating a new batabase without using our pre-made database

--- db.js
+++ db.js
@@ -6,9 +6,9 @@ const uri = "mongodb+srv://" + process.env.MONGO_DB_USERNAME + ":" + process.env
 
 mongoose.set('strictQuery', false);
 
-async function connect () {
+async function connect() {
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(uri, { dbName: 'attendy-ams' });
         console.log("Connected to the database!");
     } catch (err) {
         console.error(err);
