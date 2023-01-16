6f85c2f0be58c7965f9b09d924459e31f7b629ff
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-01-16 07:42:08 +0530

    create student collection

---
+++ controllers/StudentController.js
@@ -0,0 +1,18 @@
+const Student = require("../models/Student");
+
+//retrieve student by id
+// exports.findById = (req, res) => {
+//   console.log(req.params.id);
+//   Student.findById(req.params.id, (err, student) => {
+//     if (err) return res.status(500).send(err);
+//     return res.status(200).send(student);
+//   });
+// };
+
+//retrieve all student
+exports.findAll = (req, res) => {
+  Student.find((err, students) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(students);
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const express = require("express")
+const express = require("express");
 const router = express();
 const testRouter = require("./testRoute");
+const studentRouter = require("./student");
 
 // define routers
 router.use("/test", testRouter);
+router.use("/student", studentRouter);
 
 module.exports = router;
