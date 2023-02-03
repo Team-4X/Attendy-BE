c7aed2e4a2732924e50915594fbcf5e8def3bcee
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-02-03 12:16:57 +0530

    create functionalities for adding, deleting and finding teachers

--- controllers/StaffManageController.js
+++ controllers/StaffManageController.js
@@ -1,5 +1,65 @@
 const { generateHash, validatePassword } = require('../lib/passwordUtils');
+const Staff = require("../models/Staff");
 
 exports.register = async (req, res) => {
 	console.log("Let's register a fucking staff member.");
+}
+exports.getTeachers = async (req, res) => {
+	const teachers = await Staff.find();
+	console.log('all is well');
+	res.json(teachers);
+}
+exports.delTeachers = async (req, res) => {
+	const teacher = req.body;
+	try {
+		await Staff.findByIdAndDelete(teacher._id);
+		res.status(200).send("Successfully deleted teacher from database.");
+	} catch (error) {
+		console.error(error);
+	}
+}
+exports.addTeachers = async (req, res) => {
+
+	const saltHash = generateHash(req.body.password);
+	const salt = saltHash.salt;
+	const hash = saltHash.hash;
+
+	const newTeacher = new Staff({
+		name: req.body.name,
+		id: req.body.id,
+		salt: salt,
+		hash: hash,
+	});
+
+	try {
+		await newTeacher.save()
+		.then((teacher) => {
+			res.status(201).json({message: "Teacher added to the database Successfully!"});
+		});
+	} catch (err) {
+		console.error(err);
+	}
+}
+exports.findTeachers = async (req, res) => {
+	let searchParameter;
+	if (req.body.name == null && req.body.staffID !== null) {
+		searchParameter = req.body.staffID;
+		Staff.find({id: {$regex: searchParameter, $options: 'i'}}, (err, teacher) => {
+			if (err) console.log(err);
+			else {
+					console.log(teacher);
+					res.send({
+					data: teacher
+				});
+			}
+		});
+	} else if (req.body.staffID == null && req.body.name !== null) {
+		searchParameter = req.body.name;
+			Staff.find({name: {$regex: searchParameter, $options: 'i'}}, (err, teacher) => {
+			if (err) console.log(err);
+			else res.send({
+				data: teacher
+			});
+		});
+	}
 }
--- routes/staffManage.js
+++ routes/staffManage.js
@@ -3,5 +3,9 @@ const staffManageRouter = express.Router();
 const staffManageController = require("../controllers/StaffManageController");
 
 staffManageRouter.post("/manage", staffManageController.register);
+staffManageRouter.get("/getTeachers", staffManageController.getTeachers);
+staffManageRouter.delete("/delTeachers", staffManageController.delTeachers);
+staffManageRouter.post("/addTeachers", staffManageController.addTeachers);
+staffManageRouter.post("/findTeachers", staffManageController.findTeachers);
 
 module.exports = staffManageRouter;
