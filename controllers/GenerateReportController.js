78a557a1a87a26282cd97dca7ec0fed78887d86f
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2023-03-21 19:14:04 +0530

    test the routes

--- controllers/GenerateReportController.js
+++ controllers/GenerateReportController.js
@@ -1,3 +1,8 @@
-exports.generate = async(req, res) => {
+const StudentAttendance = require("../models/StudentAttendance");
+const StaffAttendance = require("../models/TeacherAttendance");
+const Student = require("../models/Student");
+const Staff = require("../models/Staff");
+
+exports.generateStudentReport = async(req, res) => {
     
 }
--- routes/generateReport.js
+++ routes/generateReport.js
@@ -2,6 +2,6 @@ const express = require('express');
 const generateReportRouter = express.Router();
 const generateReportController = require("../controllers/GenerateReportController");
 
-generateReportRouter.get("/download", generateReportController.generate);
+generateReportRouter.get("/student", generateReportController.generateStudentReport);
 
 module.exports = generateReportRouter;
