d7d300763a6ac17d908009279b274c10dd992475
L.A.S.S. Kularathna <sujeewasandeepa@protonmail.ch>
2023-03-25 21:03:38 +0530

    Edit index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const router = express();
 const staffRouter = require("./staffRoute");
-const attendanceRouter = require("./attendanceTeacher");
+const attendanceTeacherRouter = require("./attendanceTeacher");
+const userRouter = require("./userRoute");
+const authRouter = require('./auth');
+const manageStaffRouter = require('./staffManage');
+const attendanceRouter = require('./attendance');
+const contactUsRouter = require('./contactUs');
 
 // define routers
-router.use("/staff", attendanceRouter);
+router.use("/staff-attendance", attendanceRouter);
+router.use("/user", userRouter);
+router.use("/auth", authRouter);
+router.use("/staff", manageStaffRouter);
+router.use("/attendance", attendanceRouter);
+router.use("/contactus", contactUsRouter);
 
 module.exports = router;
